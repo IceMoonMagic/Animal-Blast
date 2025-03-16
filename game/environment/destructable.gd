5443fbb3b3c3e547a7a23b1fe141f7c8283940a7
IceMoonMagic <icemoonmagic@gmail.com>
2025-03-15 19:53:54 -0500

    Added destructables for flair

---
+++ game/environment/destructable.gd
@@ -0,0 +1,43 @@
+@tool
+
+extends Area2D
+
+## Texture2D object to draw
+@export var texture: Texture2D:
+	get:
+		return sprite.texture
+	set(val):
+		if not is_node_ready():
+			await ready
+		sprite.texture = val
+		sprite.offset.y = -val.get_height() / 2.0
+		collision_shape.a.x = -val.get_width() / 2.0
+		collision_shape.b.x = val.get_width() / 2.0
+## Weather to flip over or destroy when run over
+## [br]True: Flips[br]False: Deletes
+@export var flip_on_break: bool = true
+## Y component of collision line
+@export var collision_height: float = -10:
+	set(val):
+		if not is_node_ready():
+			await ready
+		collision_height = val
+		collision_shape.a.y = val
+		collision_shape.b.y = val
+@export var destroyed: bool = false:
+	set(val):
+		destroyed = val
+		if destroyed:
+			if not flip_on_break:
+				queue_free()
+			self.monitoring = false
+			sprite.flip_v = true
+			sprite.offset.y = sprite.texture.get_height() / 2.0 + collision_height
+		else:
+			self.monitoring = true
+			sprite.flip_v = false
+			sprite.offset.y = -sprite.texture.get_height() / 2.0
+
+
+@onready var collision_shape: SegmentShape2D = $CollisionShape2D.shape
+@onready var sprite: Sprite2D = $Sprite2D
---
+++ game/environment/environment.gd
@@ -0,0 +1,35 @@
+@tool
+extends Node2D
+
+enum Terrain {GRASS, SNOW, WATER, LAVA}
+
+
+@export var normal_tiles: Terrain = Terrain.GRASS:
+	set(tile):
+		normal_tiles = tile
+		_set_cells_between(Vector2i.ZERO, Vector2i(4, 8), normal_tiles)
+@export var lose_tiles: Terrain = Terrain.WATER:
+	set(tile):
+		lose_tiles = tile
+		_set_cells_between(Vector2i(0, 9), Vector2i(4,10), lose_tiles)
+@export var defense_tiles: Terrain = Terrain.GRASS:
+	set(tile):
+		defense_tiles = tile
+		_set_cells_between(Vector2i(0, 11), Vector2i(4, 11), defense_tiles)
+
+@onready var tile_map_layer: TileMapLayer = $TileMapLayer
+
+
+func _set_cells_between(start: Vector2i, end: Vector2i, terrain: int) -> void:
+	if not is_node_ready() and not Engine.is_editor_hint():
+		await ready
+	var cells: Array[Vector2i] = _cells_between(start, end)
+	tile_map_layer.set_cells_terrain_connect(cells, 0, terrain)
+
+
+func _cells_between(start: Vector2i, end: Vector2i) -> Array[Vector2i]:
+	var result: Array[Vector2i] = []
+	for x in range(end.x - start.x + 1):
+		for y in range(end.y - start.y + 1):
+			result.append(Vector2i(x + start.x, y + start.y))
+	return result
