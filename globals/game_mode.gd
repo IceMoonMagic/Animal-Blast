a0eb0efa007f5aba8abc5183bbe4a721285310db
IceMoonMagic <icemoonmagic@gmail.com>
2025-05-15 02:22:29 -0500

    Added saving and loading settings

--- globals/game_mode.gd
+++ globals/game_mode.gd
@@ -8,7 +8,7 @@ const DIFFICUTLY_SETTINGS: Dictionary[Difficulty, GameDifficulty] = {
 }
 const BallScene = preload("res://game/balls/ball.tscn")
 
-var animal_palette: AnimalPalette = AnimalPalette.new()
+@export_storage var animal_palette: AnimalPalette = AnimalPalette.new()
 var environment_palette: EnvironmentPalette = EnvironmentPalette.new()
 var continuous := false
 var custom_difficulty: GameDifficulty = (
@@ -26,6 +26,15 @@ var ball_radius: float:
 		return 640.0 / (2 * difficulty_settings.row_size + 1)
 
 
+#
+func _enter_tree() -> void:
+	load_config_file()
+
+
+func _exit_tree() -> void:
+	save_config_file()
+
+
 func init_ball(
 	spin_direction: int = [-1, 1].pick_random(),
 ) -> Ball:
@@ -38,3 +47,70 @@ func init_ball(
 	ball.radius = ball_radius
 	ball.constant_angular_velocity = spin_direction
 	return ball
+
+
+func load_config_file() -> Error:
+	var config_file := ConfigFile.new()
+	var err := config_file.load("user://config.cfg")
+	if err != OK:
+		return err
+
+	animal_palette.animals = config_file.get_value(
+		"GameMode", "animals", animal_palette.animals
+	)
+	var env_palette: Array = config_file.get_value(
+		"GameMode", "tiles", [0, 2, 0]
+	)
+	environment_palette.normal_tiles = (
+		env_palette[0] as EnvironmentPalette.Terrain
+	)
+	environment_palette.lose_tiles = (
+		env_palette[1] as EnvironmentPalette.Terrain
+	)
+	environment_palette.defense_tiles = (
+		env_palette[2] as EnvironmentPalette.Terrain
+	)
+	continuous = config_file.get_value("GameMode", "continuous", continuous)
+
+	var cust_diff: Array = config_file.get_value(
+		"GameMode",
+		"custom_difficulty",
+		[
+			custom_difficulty.allowed_strikes,
+			custom_difficulty.continuous_speed,
+			custom_difficulty.palette_size,
+			custom_difficulty.row_size
+		]
+	)
+	custom_difficulty.allowed_strikes = roundi(cust_diff[0])
+	custom_difficulty.continuous_speed = cust_diff[1]
+	custom_difficulty.palette_size = roundi(cust_diff[2])
+	custom_difficulty.row_size = roundi(cust_diff[3])
+
+	return OK
+
+
+func save_config_file() -> Error:
+	var config_file := ConfigFile.new()
+	config_file.set_value("GameMode", "animal_palette", animal_palette.animals)
+	config_file.set_value(
+		"GameMode",
+		"tiles",
+		[
+			environment_palette.normal_tiles,
+			environment_palette.lose_tiles,
+			environment_palette.defense_tiles
+		]
+	)
+	config_file.set_value("GameMode", "continuous", continuous)
+	config_file.set_value(
+		"GameMode",
+		"custom_difficulty",
+		[
+			custom_difficulty.allowed_strikes,
+			custom_difficulty.continuous_speed,
+			custom_difficulty.palette_size,
+			custom_difficulty.row_size
+		]
+	)
+	return config_file.save("user://config.cfg")
